560088e0a71011731e6ef6d4f00896619e5ee4d0
Jasil <jasilmeledath@gmail.com>
2026-06-26 14:48:42 +0530

    Include user schema in analytics module

--- src/modules/analytics/analytics.module.ts
+++ src/modules/analytics/analytics.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AnalyticsService } from './analytics.service';
+import { User, UserSchema } from '../users/schemas/user.schema';
 import { AnalyticsController } from './analytics.controller';
 import { Lead, LeadSchema } from '../leads/schemas/lead.schema';
 import { Activity, ActivitySchema } from '../activities/schemas/activity.schema';
@@ -12,6 +13,7 @@ import { FollowUp, FollowUpSchema } from '../follow-ups/schemas/follow-up.schema
       { name: Lead.name, schema: LeadSchema },
       { name: Activity.name, schema: ActivitySchema },
       { name: FollowUp.name, schema: FollowUpSchema },
+      { name: User.name, schema: UserSchema },
     ]),
   ],
   controllers: [AnalyticsController],
